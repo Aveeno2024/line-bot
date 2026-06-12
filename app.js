d1be8307afe76021e9d379a22227ea3c69595ec5
Aveeno2024 <159961758+Aveeno2024@users.noreply.github.com>
2026-06-13 03:51:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // ⚠️ 請填入你的金鑰 ⚠️
 // ==========================================
 const CHANNEL_ACCESS_TOKEN = 'FpYYGobL5CFc3u5lsVOEGfHTSEYHHiw7P3e25FD5MhqusbsANf98WzgO2eAvPXBSkcLFdA8uI5pjbAZ75WX/xIcmlNcjUEztbyBvT0f8Z9y6QgmS/F+EPNDkUgO2YsRBdpKhRv5J3Eh0PIfF6kt4QwdB04t89/1O/w1cDnyilFU=';
-const CWA_API_KEY = CWA-B59372C7-9BD4-44F8-B759-D6ED723C6BC4';
+const CWA_API_KEY = 'CWA-B59372C7-9BD4-44F8-B759-D6ED723C6BC4';
 // ==========================================
 
 // GitHub 設定
